0ae640874bbf86cca419a4893a9a386518dd77f5
Mark Alston <malston@vmware.com>
2024-02-02 15:06:00 -0700

    Add support for gitlab

--- hack/install.sh
+++ hack/install.sh
@@ -4,11 +4,15 @@ set -o errexit
 
 __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-brew update
-brew upgrade kubefirst || brew install kubefirst/tools/kubefirst
-brew upgrade mkcert || brew install mkcert
-brew upgrade nss || brew install nss
-mkcert -install
+function usage() {
+    echo "Usage:"
+    echo "  $0 [flags]"
+    printf "\n"
+    echo "Flags:"
+    printf "  %s, --provider\tGit provider\n" "-p"
+    printf "  %s, --skip-tools\tDo not install (kubefirst, mkcert, nss)\n" "-s"
+    printf "\n"
+}
 
 function read_password() {
   local prompt="$1"
@@ -23,19 +27,84 @@ function read_password() {
   echo "$password"
 }
 
-GITHUB_TOKEN=$1
+INSTALL_TOOLS=true
 
-if [[ -z $GITHUB_TOKEN ]]; then
-  GITHUB_TOKEN=$(read_password "Enter a Github Personal Access Token: ")
+# parse argv variables
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+  -g | -p | --provider | --git-provider)
+    GIT_PROVIDER="$2"
+    shift 2
+    ;;
+  -g=* | --git-provider=* | -p=* | --provider=*)
+    GIT_PROVIDER="${1#*=}"
+    shift 1
+    ;;
+  -s | --skip | --skip-tools)
+    INSTALL_TOOLS=false
+    shift 1
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    echo "Unknown option: $1"
+    usage
+    exit 1
+    ;;
+  esac
+done
+
+if [[ -z $GIT_PROVIDER ]]; then
+  echo "Enter your git provider (github, gitlab): "
+  read -r GIT_PROVIDER
 fi
 
-# k3d cluster create gimlet-cluster --k3s-arg "--disable=traefik@server:0"
+if [[ $INSTALL_TOOLS = true ]]; then
+  brew update
+  brew upgrade kubefirst || brew install kubefirst/tools/kubefirst
+  brew upgrade mkcert || brew install mkcert
+  brew upgrade nss || brew install nss
+  mkcert -install
+fi
 
-kubefirst k3d create \
-  --cluster-name kubefirst \
-  --cluster-type mgmt \
-  --github-user malston \
-  --git-provider github
+case $GIT_PROVIDER in
+  github)
+    if [[ -z $GITHUB_TOKEN ]]; then
+      GITHUB_TOKEN=$(read_password "Enter a Github Personal Access Token: ")
+      export GITHUB_TOKEN
+    fi
+    if ! ssh-keyscan -t rsa github.com &> /dev/null; then
+      ssh-keyscan github.com >> ~/.ssh/known_hosts
+    fi
+    kubefirst k3d create \
+      --cluster-name kubefirst \
+      --cluster-type mgmt \
+      --github-user malston \
+      --git-provider github
+    ;;
+  gitlab)
+    if [[ -z $GITLAB_TOKEN ]]; then
+      GITLAB_TOKEN=$(read_password "Enter a Gitlab Personal Access Token: ")
+    fi
+    if ! ssh-keyscan -t rsa gitlab.com &> /dev/null; then
+      ssh-keyscan gitlab.com >> ~/.ssh/known_hosts
+    fi
+    export GITLAB_TOKEN
+    kubefirst k3d create \
+      --cluster-name kubefirst \
+      --cluster-type mgmt \
+      --github-user marktalston \
+      --gitlab-group marsguitars \
+      --git-provider gitlab
+    ;;
+  *)
+    echo "Unknown value for '--git-provider'"
+    usage
+    exit 1
+    ;;
+esac
 
 kubefirst k3d root-credentials
 
@@ -63,5 +132,5 @@ echo  URL: https://argocd.kubefirst.dev
 echo  --- Vault ------------------------------------------------------------
 echo  URL: https://vault.kubefirst.dev
 
-git clone --recursive https://github.com/malston/gitops.git "$__DIR/../gitops"
-git clone --recursive https://github.com/malston/metaphor.git "$__DIR/../metaphor"
+# git clone --recursive https://github.com/malston/gitops.git "$__DIR/../gitops"
+# git clone --recursive https://github.com/malston/metaphor.git "$__DIR/../metaphor"
